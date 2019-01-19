1ca71ce157d79a11b37bfcdd1d3fb143d19415b2
Rod Johnson <johnsonroda@gmail.com>
2019-01-18 18:46:42 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/jd/Jd1Application.java
@@ -0,0 +1,12 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jd1Application.class, args);
+	}
+}
